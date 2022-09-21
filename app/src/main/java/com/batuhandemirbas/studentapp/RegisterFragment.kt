5877b9cadc95b3abe9da63cd0feb2b33f9269da1
Batuhan Demirbaş <batuhan@demirbas.co>
2022-09-21 13:35:10 +0300

    Register structure changed

--- app/src/main/java/com/batuhandemirbas/studentapp/RegisterFragment.kt
+++ app/src/main/java/com/batuhandemirbas/studentapp/RegisterFragment.kt
@@ -7,17 +7,27 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import androidx.navigation.Navigation
-import com.batuhandemirbas.studentapp.data.DatabaseHelper
 import com.batuhandemirbas.studentapp.databinding.FragmentRegisterBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterFragment : Fragment() {
 
+    private lateinit var auth: FirebaseAuth
     private var _binding: FragmentRegisterBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,29 +41,15 @@ class RegisterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Connect database
-        val db = DatabaseHelper(context)
-
         // Actions to be applied when the register button is pressed
         val toggleButton = binding.passwordToggle
         binding.registerButton.setOnClickListener {
 
             if (toggleButton.isChecked) {
-                val number = binding.numberTextField.text.toString().toInt()
-                val name = binding.nameTextField.text.toString()
-                val password = binding.passwordTextField.text.toString().toInt()
+                val email = binding.emailTextField.text.toString()
+                val password = binding.passwordTextField.text.toString()
 
-                // Write data
-                db.writeData(number, name, password, context)
-
-
-                // Snackbar message
-                Snackbar.make(it, "Başarıyla kayıt oldunuz.", Snackbar.LENGTH_SHORT).show()
-                binding.passwordTextField.onEditorAction(EditorInfo.IME_ACTION_DONE)
-
-                // Action to LoginFragment
-                val action = RegisterFragmentDirections.actionRegisterFragmentToLoginFragment()
-                Navigation.findNavController(it).navigate(action)
+                signUpFirebase(email, password, it)
 
             } else {
                 // Not cheked snackbar message
@@ -61,6 +57,27 @@ class RegisterFragment : Fragment() {
                 binding.passwordTextField.onEditorAction(EditorInfo.IME_ACTION_DONE)
             }
         }
+    }
+
+    private fun signUpFirebase(email: String, password: String, view: View,) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = auth.currentUser
+                    // Snackbar message
+                    Snackbar.make(view, "Başarıyla kayıt oldunuz.", Snackbar.LENGTH_SHORT).show()
+                    binding.passwordTextField.onEditorAction(EditorInfo.IME_ACTION_DONE)
+
+                    // Action to LoginFragment
+                    val action = RegisterFragmentDirections.actionRegisterFragmentToLoginFragment()
+                    Navigation.findNavController(view).navigate(action)
+                } else {
+                    // Snackbar message
+                    Snackbar.make(view, "Kayıt olamadınız.", Snackbar.LENGTH_SHORT).show()
+                    binding.passwordTextField.onEditorAction(EditorInfo.IME_ACTION_DONE)
+                }
+            }
     }
 
     override fun onDestroyView() {
